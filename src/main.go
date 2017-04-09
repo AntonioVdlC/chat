82f7ca993b1a398112642fd544dcd81c40e05f17
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-04-09 20:11:36 +0200

    Add login with Facebook
    This breaks the UI, but hey, at least know I have the auth more or less under control 😅 ...

--- src/main.go
+++ src/main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"html/template"
 	"log"
+	"math"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/sessions"
 	"github.com/gorilla/websocket"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/facebook"
 )
 
 var upgrader = websocket.Upgrader {
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
+}
+
+var port = getPort()
+
+func init() {
+	store := sessions.NewFilesystemStore(os.TempDir(), []byte("goth-example1"))
+	store.MaxLength(math.MaxInt64)
+
+	gothic.Store = store
+
+	goth.UseProviders(facebook.New(os.Getenv("FACEBOOK_KEY"), os.Getenv("FACEBOOK_SECRET"), "http://localhost" + port + "/auth/callback?provider=facebook"))
 }
 
 func main() {
@@ -23,9 +41,11 @@ func main() {
 		serveWs(hub, w, r)
 	})
 
-	port := getPort()
-	log.Printf("Listening on port %s", port)
+	http.HandleFunc("/auth/callback", authCallback)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/auth", auth)
 
+	log.Printf("Listening on port %s", port)
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServer: ", err)
@@ -49,3 +69,42 @@ func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
 	go client.write()
 	client.read()
 }
+
+func authCallback(w http.ResponseWriter, r *http.Request) {
+	user, err := gothic.CompleteUserAuth(w, r)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	t, _ := template.New("foo").Parse(userTemplate)
+	t.Execute(w, user)
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	gothic.Logout(w, r)
+	w.Header().Set("Location", "/")
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func auth(w http.ResponseWriter, r *http.Request) {
+	if user, err := gothic.CompleteUserAuth(w, r); err == nil {
+		t, _ := template.New("foo").Parse(userTemplate)
+		t.Execute(w, user)
+	} else {
+		gothic.BeginAuthHandler(w, r)
+	}
+}
+
+var userTemplate = `
+<p><a href="/logout/{{.Provider}}">logout</a></p>
+<p>Name: {{.Name}} [{{.LastName}}, {{.FirstName}}]</p>
+<p>Email: {{.Email}}</p>
+<p>NickName: {{.NickName}}</p>
+<p>Location: {{.Location}}</p>
+<p>AvatarURL: {{.AvatarURL}} <img src="{{.AvatarURL}}"></p>
+<p>Description: {{.Description}}</p>
+<p>UserID: {{.UserID}}</p>
+<p>AccessToken: {{.AccessToken}}</p>
+<p>ExpiresAt: {{.ExpiresAt}}</p>
+<p>RefreshToken: {{.RefreshToken}}</p>
+`
