6307333a1aefcc69e7846434e145f7dabdb2a09f
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-04-22 17:27:44 +0200

    Remove 'mine' and 'others' classes on chat messages

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -5,15 +5,14 @@ new Vue({
     ws: null,
     message: '',
     chatContent: '',
-    id: '{{.UserID}}',
   },
 
   created: function() {
     this.ws = new WebSocket("ws://" + window.location.host + "/ws")
     this.ws.addEventListener('message', (e) => {
-      let { id, user, avatar, content } = JSON.parse(e.data)
+      let { user, avatar, content } = JSON.parse(e.data)
       // This may need to be re-written sometime in the near future ... :P
-      this.chatContent += "<li class='" + ((id === this.id) ? "mine" : "others") + "'><img class='avatar' src='" + avatar + "' alt='" + user + "' title='" + user + "'><span class='message'>" + content + "</span></li>"
+      this.chatContent += "<li><img class='avatar' src='" + avatar + "' alt='" + user + "' title='" + user + "'><span class='message'>" + content + "</span></li>"
     })
   },
 
