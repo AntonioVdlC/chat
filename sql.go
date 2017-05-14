c2bb110069f4215d16370d2223631998bbde1136
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-05-14 23:32:41 +0200

    Include users that just logged in for the first time

--- sql.go
+++ sql.go
@@ -118,22 +118,23 @@ func selectConnectedUsers(db *sql.DB, userID string) (*sql.Rows, error) {
 		FROM messages m 
 			INNER JOIN
 
-			(SELECT DISTINCT user_id, user_name, user_avatar, MAX(date_post)
-			FROM messages
-			WHERE type = 'logout'
-			GROUP BY user_id, user_name, user_avatar) users_logout 
-			ON m.user_id = users_logout.user_id
-
-			INNER JOIN 
-
-			(SELECT DISTINCT user_id, user_name, user_avatar, MAX(date_post)
+			(SELECT DISTINCT user_id, MAX(date_post)
 			FROM messages
 			WHERE type = 'login'
-			GROUP BY user_id, user_name, user_avatar) users_login 
+			GROUP BY user_id) users_login 
 			ON m.user_id = users_login.user_id
-				AND users_login.max > users_logout.max
+
+			LEFT JOIN 
+
+			(SELECT DISTINCT user_id, MAX(date_post)
+			FROM messages
+			WHERE type = 'logout'
+			GROUP BY user_id) users_logout 
+			ON m.user_id = users_logout.user_id
 
 		WHERE m.user_id != $1
+			AND users_login.max > users_logout.max 
+					OR users_logout.max IS NULL
 	`
 
 	rows, err := db.Query(stmt, userID)
